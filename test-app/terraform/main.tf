ec3852f205884ba4918eb5b98546025dc87d8945
Matthew Beckett <matt@beckett.cloud>
2022-04-27 20:35:51 +0100

    Remove extra resource definitions present in app/main.tf

--- test-app/terraform/main.tf
+++ test-app/terraform/main.tf
@@ -5,37 +5,6 @@ data "archive_file" "source" {
     excludes = fileset("${var.source_path}/terraform", "*")
 }
 
-resource "google_project_service" "cloudfunctions_api" {
-  provider = google-beta
-  project = var.project_id
-
-  service  = "cloudfunctions.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "cloudrun_api" {
-  provider = google-beta
-  project = var.project_id
-
-  service  = "run.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "artifactregistry_api" {
-  provider = google-beta
-  project = var.project_id
-
-  service  = "artifactregistry.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "cloudbuild_api" {
-  provider = google-beta
-  project = var.project_id
-
-  service  = "cloudbuild.googleapis.com"
-  disable_on_destroy = false
-}
 
 resource "google_storage_bucket" "test_app_gcs_repo" {
   project = var.project_id
